e6d9f3787a4f76e51bcc4f37b8f6a9d43a58d811
skezlica <stefannasic00@gmail.com>
2024-01-08 11:12:08 +0100

    Added variables for catching values from input fields and selected operator, and one switch for calculating result. Also, added required parameter in the input fields.

--- index.php
+++ index.php
@@ -3,7 +3,30 @@
 $result = '';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $first_number = $_POST['fn'];
+    $seccond_number = $_POST['sn'];
+    $operator = $_POST['operator'];
+    
+    switch($operator) {
+        case '+':
+            $result = $first_number + $seccond_number;
+            break;
 
+        case '-':
+            $result = $first_number - $seccond_number;
+            break;
+
+        case '*':
+            $result = $first_number * $seccond_number;
+            break;
+
+        case '/':
+            $result = $first_number / $seccond_number;
+            break;
+        
+        default:
+            $result = "No operator found";
+    }
 }
 
 ?>
@@ -24,10 +47,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php echo $result ?> <br>
 
         <label for="fn_id">First Number:</label>
-        <input type="number" name="fn" id="fn_id"> <br>
+        <input type="number" name="fn" id="fn_id" required> <br>
 
         <label for="sn_id">Seccond Number:</label>
-        <input type="number" name="sn" id="sn_id"> <br>
+        <input type="number" name="sn" id="sn_id" required> <br>
 
         <select name="operator" id="">
             <option value="+">+</option>
